dc7f8be79eafec3b681f50b41a58d321d6a9a942
Antony <vontonnie@gmail.com>
2021-07-25 14:42:31 +0300

    Add Padding

--- app/src/main/java/com/uxstate/MainActivity.kt
+++ app/src/main/java/com/uxstate/MainActivity.kt
@@ -3,12 +3,15 @@ package com.uxstate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uxstate.ui.theme.ComposeIntroTheme
 
@@ -31,7 +34,7 @@ fun Greeting(name: String) {
 
     Surface(color = Color.Red){
 
-        Text(text = "Hello $name!")
+        Text(text = "Hello $name!" ,modifier = Modifier.padding(8.dp))
     }
 
 }
